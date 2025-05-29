ec167eda4c45d1d1986c13a1cdbf856d2b551ea4
AdamB25 <barsialtunit@gmail.com>
2025-05-29 18:37:03 -0400

    use config.json file in the tests

---
+++ tests/test_login_with_config.py
@@ -0,0 +1,31 @@
+import time
+
+import pytest
+
+from pages.login_page import LoginPage
+from utils.configuration_reader import read_config
+
+
+@pytest.fixture
+def browser():
+    # set up
+    from utils.driver_factory import get_driver
+    driver=get_driver()
+
+    yield driver
+
+    # tear down
+
+    time.sleep(5)
+    driver.quit()
+
+
+def test_valid_login(browser):
+    login_page=LoginPage(browser)
+    login_page.load()
+    config_data=read_config()
+    username=config_data["login"]["username"]
+    password=config_data["login"]["password"]
+    login_page.login(username,password)
+    login_page.is_login_successful()
+
---
+++ utils/configuration_reader.py
@@ -0,0 +1,7 @@
+import json
+
+
+def read_config():
+    with open("../config.json","r") as file:
+        config_data=json.load(file)
+    return config_data
